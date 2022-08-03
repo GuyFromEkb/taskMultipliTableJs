3ec49762726ec74fd5d41a3fdeb9ec9b3d31a5e9
Sergey <graygmai1@gmail.com>
2022-08-03 18:02:05 +0500

    rework

--- script.js
+++ script.js
@@ -1,109 +1,67 @@
-const NUMBER = 45
+const NUMBER = 6
 
 multipliTable(NUMBER)
 
-function multipliTable(num) {
 
-    if (typeof (num) !== "number" || num < 1) {
+function multipliTable(numb) {
+
+    if (typeof (numb) !== "number" || numb < 1) {
         console.log('Нужно ввести число, которое больше 0!')
         return
     }
 
-    //массив массивов таблицы умножения до заданного числа
-    const numArr = createArray(num)
-    // console.log(numArr)
+    for (let i = 1; i <= numb; i++) {
 
-    //массив максимальной длины числа в столбце, индекс соответствует индексу в массиве таблицы умножения
-    const spaceCountArr = countSpaceForDigit(numArr)
-    // console.log(spaceCountArr)
+        let rowStr = ""
+        let header = ""
 
-    //массив массивов таблицы умножения с выровненными по правому краю значениями
-    const alignDigitArr = alignDigitToRight(numArr, spaceCountArr)
-    // console.log(alignDigitArr)
+        for (let j = 1; j <= numb; j++) {
 
-    //вывод в консоль
-    renderToConsole(alignDigitArr)
-}
+            if (i === 1) {
+                header += printNumb(i, j, numb) + " "
+            }
+            if (j === 1) {
+                rowStr += printNumb(i, j, numb) + "|"
+            }
 
-function createArray(num) {
-    let buffArr = []
-
-    for (let i = 1; i <= num; i++) {
-
-        let strDigit = ''
-
-        for (let j = 1; j <= num; j++) {
-            strDigit += " " + (i * j)
-        }
-        buffArr.push(strDigit)
-    }
-
-    //создать массив без первого индекса (пустой разделитель)
-    const MainArr = buffArr.map(item => item.split(' ').slice(1))
-
-    return (MainArr)
-}
-
-function countSpaceForDigit(numArr) {
-    const arr = [...numArr]
-
-    const spaceCountArr = arr.reverse()[0].map(item => item.length)
-
-    return spaceCountArr
-}
-
-function alignDigitToRight(numArr, spaceCountArr) {
-
-    const alignDigitArr = numArr.map(row => {
-        return (
-            row.map((digit, index) =>
-            (   //если длинна цифры меньше чем максимальная длинна цифры в строке, то прибавить кол-во пробелов для выравнивания 
-                //или вернуть цифру без изменения
-                digit.length < spaceCountArr[index] ? (" ".repeat(spaceCountArr[index] - digit.length)) + digit : digit)
-            )
-        )
-    })
-    return alignDigitArr
-}
-
-function renderToConsole(alignDigitArr) {
-    //максимальная длина цифр в 1 колонке
-    const firstColumnLength = alignDigitArr.length.toString().length
-
-    alignDigitArr.forEach((item, index) => {
-
-        //первая цифра в колонке + |
-        const firstColumnDigit = firstColumRenderToConsole(index, firstColumnLength)
-
-        //разделительная полоса
-        if (index === 0) {
-            firstRowRenderToConsole(alignDigitArr, firstColumnLength, item)
+            rowStr += printNumb(i, j, numb) + " "
         }
 
-        console.log(firstColumnDigit, item.join(' '))
-    })
+        if (header) {
+            printHeader(i, numb, header)
+        }
 
-}
-
-function firstColumRenderToConsole(index, firstColumnLength) {
-    //индексация начало с 1
-    const i = index + 1
-    //если номер "короче", чем макс длина цифр в колонке то прибавить недостающие кол-во пробелов + разделитель |
-    if (i.toString().length < firstColumnLength) {
-        return " ".repeat(firstColumnLength - i.toString().length) + i + '|'
+        console.log(rowStr)
     }
 
-    return i.toString() + '|'
 }
 
-function firstRowRenderToConsole(numArr, firstColumnLength, firstRowArr) {
-    //получаем общую максимальную длину символов строке, без пробелов
-    const rowLengthWithoutSpace = numArr[numArr.length - 1].join('').length
+function printNumb(rowNumb, tableNum, numb) {
 
-    // отступить весь 1ый столбец + полоска (1) + пробел от края (1) + вся первая строка для нумерации
-    console.log(" ".repeat(firstColumnLength + 2) + firstRowArr.join(' '))
+    return " ".repeat(getLength(rowNumb, tableNum, numb)) + (rowNumb * tableNum)
+}
 
-    //количество  символов в строке + количество пробелов + отступ слева + пробел (1)
-    const repeatNumbForLine = rowLengthWithoutSpace + numArr.length + (firstColumnLength + 1)
-    console.log('-'.repeat(repeatNumbForLine))
+function getLength(rowNumb, tableNum, numb) {
+
+    const lengthTable = (rowNumb * tableNum).toString().length
+    const maxLengthTable = (tableNum * numb).toString().length
+
+    return maxLengthTable - lengthTable
+}
+
+function printHeader(rowNumb, numb, headerStr) {
+
+    
+    const headerSpaceLength = (numb * rowNumb).toString().length
+
+
+    let strLine = numb.toString()
+
+    for (let i = 1; i <= numb; i++) {
+
+        strLine += i * numb
+    }
+
+    console.log(" ".repeat(headerSpaceLength + 1) + headerStr)
+    console.log('-'.repeat(strLine.length + numb))
 }
