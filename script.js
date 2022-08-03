cb201f48a07fb0ad1cacb87339140c698980ae47
Sergey <graygmai1@gmail.com>
2022-08-03 18:05:27 +0500

    upd

--- script.js
+++ script.js
@@ -2,66 +2,55 @@ const NUMBER = 6
 
 multipliTable(NUMBER)
 
-
 function multipliTable(numb) {
+	if (typeof numb !== "number" || numb < 1) {
+		console.log("Нужно ввести число, которое больше 0!")
+		return
+	}
 
-    if (typeof (numb) !== "number" || numb < 1) {
-        console.log('Нужно ввести число, которое больше 0!')
-        return
-    }
+	for (let i = 1; i <= numb; i++) {
+		let rowStr = ""
+		let header = ""
 
-    for (let i = 1; i <= numb; i++) {
+		for (let j = 1; j <= numb; j++) {
+			if (i === 1) {
+				header += printNumb(i, j, numb) + " "
+			}
+			if (j === 1) {
+				rowStr += printNumb(i, j, numb) + "|"
+			}
 
-        let rowStr = ""
-        let header = ""
+			rowStr += printNumb(i, j, numb) + " "
+		}
 
-        for (let j = 1; j <= numb; j++) {
+		if (header) {
+			printHeader(i, numb, header)
+		}
 
-            if (i === 1) {
-                header += printNumb(i, j, numb) + " "
-            }
-            if (j === 1) {
-                rowStr += printNumb(i, j, numb) + "|"
-            }
-
-            rowStr += printNumb(i, j, numb) + " "
-        }
-
-        if (header) {
-            printHeader(i, numb, header)
-        }
-
-        console.log(rowStr)
-    }
-
+		console.log(rowStr)
+	}
 }
 
 function printNumb(rowNumb, tableNum, numb) {
-
-    return " ".repeat(getLength(rowNumb, tableNum, numb)) + (rowNumb * tableNum)
+	return " ".repeat(getLength(rowNumb, tableNum, numb)) + rowNumb * tableNum
 }
 
 function getLength(rowNumb, tableNum, numb) {
+	const lengthTable = (rowNumb * tableNum).toString().length
+	const maxLengthTable = (tableNum * numb).toString().length
 
-    const lengthTable = (rowNumb * tableNum).toString().length
-    const maxLengthTable = (tableNum * numb).toString().length
-
-    return maxLengthTable - lengthTable
+	return maxLengthTable - lengthTable
 }
 
 function printHeader(rowNumb, numb, headerStr) {
+	const headerSpaceLength = (numb * rowNumb).toString().length
 
-    
-    const headerSpaceLength = (numb * rowNumb).toString().length
+	let strLine = numb.toString()
 
+	for (let i = 1; i <= numb; i++) {
+		strLine += i * numb
+	}
 
-    let strLine = numb.toString()
-
-    for (let i = 1; i <= numb; i++) {
-
-        strLine += i * numb
-    }
-
-    console.log(" ".repeat(headerSpaceLength + 1) + headerStr)
-    console.log('-'.repeat(strLine.length + numb))
+	console.log(" ".repeat(headerSpaceLength + 1) + headerStr)
+	console.log("-".repeat(strLine.length + numb))
 }
