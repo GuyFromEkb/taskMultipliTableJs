16cd550f328ae3597892657394036fc1297e7c69
Sergey <95606359+GuyFromEkb@users.noreply.github.com>
2022-08-12 05:19:26 +0500

    Update finaltask.js

--- finaltask.js
+++ finaltask.js
@@ -3,7 +3,7 @@ console.log(calcBalls([1, 1, 2, 1], 2))                 //mustBeAnsw : [ 1, 0, 1
 console.log(calcBalls([100, 300, 200], 3))              //mustBeAnsw : [ 0, 2, 1 ]
 console.log(calcBalls([100, 300, 200], 120))            //mustBeAnsw : [20, 60, 40]
 console.log(calcBalls([100, 300, 200, 700, 140], 0))    //mustBeAnsw : 0
-console.log(calcBalls([0, 0, 0], 0))                      //mustBeAnsw : 0
+console.log(calcBalls([0, 0, 0], 0))                    //mustBeAnsw : 0
 console.log(calcBalls([0, 0, 2], 3))                    //mustBeAnsw : [0, 0, 2]
 
 function calcBalls(arrBalls, maxWeight) {
