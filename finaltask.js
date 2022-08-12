16758807afa7f3a8f25478b40dd68c01a5eeddc3
Sergey <95606359+GuyFromEkb@users.noreply.github.com>
2022-08-12 16:42:56 +0500

    Update finaltask.js

--- finaltask.js
+++ finaltask.js
@@ -1,68 +1,58 @@
+console.log(calcBalls([100, 300, 200], 120))            //mustBeAnsw : [20, 60, 40]
+console.log(calcBalls([1, 2, 1, 1], 1))                 //mustBeAnsw : [ 0, 1, 0, 0]
 console.log(calcBalls([10, 10, 10], 100))               //mustBeAnsw : [ 10, 10, 10]
 console.log(calcBalls([1, 1, 2, 1], 2))                 //mustBeAnsw : [ 1, 0, 1, 0 ]
 console.log(calcBalls([100, 300, 200], 3))              //mustBeAnsw : [ 0, 2, 1 ]
-console.log(calcBalls([100, 300, 200], 120))            //mustBeAnsw : [20, 60, 40]
 console.log(calcBalls([100, 300, 200, 700, 140], 0))    //mustBeAnsw : 0
 console.log(calcBalls([0, 0, 0], 0))                    //mustBeAnsw : 0
-console.log(calcBalls([0, 0, 2], 3))                    //mustBeAnsw : [0, 0, 2]
+console.log(calcBalls([0, 0, 2], 3))                    //mustBeAnsw : [ 0, 0, 2 ]
 
 function calcBalls(arrBalls, maxWeight) {
+    // проверка на нули в условии
+    if (chekZero(arrBalls, maxWeight)) {
+        return 0
+    }
 
-	// проверка на нули в условии
-	if (chekZero(arrBalls, maxWeight)) {
-		return 0
-	}
+    const ballsSum = arrBalls.reduce((acc, item) => {
+        acc += item
+        return acc
+    }, 0)
 
-	const ballsSum = arrBalls.reduce((acc, item) => {
-		acc += item
-		return acc
-	}, 0)
+    const ballsProcent = arrBalls.reduce((acc, item) => {
+        acc.push(item * 100 / ballsSum)
+        return acc
+    }, [])
 
-	const ballsProcent = arrBalls.reduce((acc, item) => {
-		acc.push(item * 100 / ballsSum)
-		return acc
-	}, [])
+    // что бы не положить больше чем есть)
+    maxWeight = ballsSum < maxWeight ? ballsSum : maxWeight
+    let totalLoot = 0
 
-	// что бы не положить больше чем есть)
-	maxWeight = ballsSum < maxWeight ? ballsSum : maxWeight
-	let totalLoot = 0
+    const ballsTheifBag = ballsProcent.map(item => {
+        totalLoot += Math.floor(maxWeight * item / 100)
 
-	const ballsTheifBag = ballsProcent.map(item => {
-		const procent = Math.floor(item)
+        return Math.floor(maxWeight * item / 100)
+    })
 
-		totalLoot += Math.round(maxWeight * procent / 100)
+    if (totalLoot < maxWeight) {
+        // доложить шары, если есть место
+        addBallsToBag(ballsTheifBag, ballsProcent, maxWeight, totalLoot)
+    }
 
-		return Math.round(maxWeight * procent / 100)
-	})
-
-	if (totalLoot < maxWeight) {
-		// доложить шары, если есть место
-		addBallToZero(ballsTheifBag, ballsProcent, maxWeight, totalLoot)
-	}
-
-	return ballsTheifBag
+    return ballsTheifBag
 }
 
-function addBallToZero(ballsTheifBag, ballsProcent, maxWeight, totalLoot,) {
+function addBallsToBag(ballsTheifBag, ballsProcent, maxWeight, totalLoot,) {
+    const buffArrWithClearProcent = ballsProcent.map(item => maxWeight * item / 100)
 
-	for (let i = 0; i < ballsTheifBag.length && maxWeight - totalLoot !== 0; i++) {
+    while (maxWeight - totalLoot !== 0) {
+        const indexMostProcent = buffArrWithClearProcent.indexOf(Math.max(...buffArrWithClearProcent))
 
-		if (ballsTheifBag[i] === 0) {
-			ballsTheifBag[i] = 1
-			totalLoot++
-
-			continue
-		}
-		const buffProcent = Math.round(ballsProcent[i])
-		const buffBall = Math.round(maxWeight * buffProcent / 100)
-
-		if (buffBall - ballsTheifBag[i] === 1) {
-			ballsTheifBag[i] = buffBall
-			totalLoot++
-		}
-	}
+        ballsTheifBag[indexMostProcent] += 1
+        buffArrWithClearProcent[indexMostProcent] = 0
+        totalLoot++
+    }
 }
 
 function chekZero(arrBalls, maxWeight) {
-	return (arrBalls === 0 || maxWeight === 0 || Math.max(...arrBalls) === 0) ? true : false
+    return (arrBalls === 0 || maxWeight === 0 || Math.max(...arrBalls) === 0) ? true : false
 }
