bfeb358bd034dd849b5d8b41cae05e67e5aeed04
Sergey <95606359+GuyFromEkb@users.noreply.github.com>
2022-08-13 21:31:48 +0500

    Update finaltask.js

--- finaltask.js
+++ finaltask.js
@@ -1,12 +1,13 @@
-console.log(calcBalls([100, 300, 200], 120))            //mustBeAnsw : [20, 60, 40]
-console.log(calcBalls([1, 2, 1, 1], 1))                 //mustBeAnsw : [ 0, 1, 0, 0]
-console.log(calcBalls([10, 10, 10], 100))               //mustBeAnsw : [ 10, 10, 10]
-console.log(calcBalls([1, 1, 2, 1], 2))                 //mustBeAnsw : [ 1, 0, 1, 0 ]
-console.log(calcBalls([100, 300, 200], 3))              //mustBeAnsw : [ 0, 2, 1 ]
-console.log(calcBalls([100, 300, 200, 700, 140], 0))    //mustBeAnsw : 0
-console.log(calcBalls(0, 25))                           //mustBeAnsw : 0
-console.log(calcBalls([1, 1, 7], 3))                    //mustBeAnsw : [ 0, 0, 3 ]
-console.log(calcBalls([1, 1, 4], 3))                    //mustBeAnsw : [ 1, 0, 2 ]
+console.log(calcBalls([100, 300, 200], 120))                //mustBeAnsw : [20, 60, 40]
+console.log(calcBalls([1, 2, 1, 1], 1))                     //mustBeAnsw : [10, 1, 0, 0]
+console.log(calcBalls([10, 10, 10], 100))                   //mustBeAnsw : [10, 10, 10]
+console.log(calcBalls([1, 1, 2, 1], 2))                     //mustBeAnsw : [1, 0, 1, 0]
+console.log(calcBalls([100, 300, 200], 3))                  //mustBeAnsw : [0, 2, 1]
+console.log(calcBalls([100, 300, 200, 700, 140], 0))        //mustBeAnsw : 0
+console.log(calcBalls(0, 25))                               //mustBeAnsw : 0
+console.log(calcBalls([1, 1, 4], 3))                        //mustBeAnsw : [1, 0, 2]
+console.log(calcBalls([1, 1, 7], 3))                        //mustBeAnsw : [1, 0, 2]
+console.log(calcBalls([24, 25, 16, 25], 9))                 //mustBeAnsw : [2, 3, 2, 2]
 
 
 function calcBalls(arrBalls, maxWeight) {
@@ -21,7 +22,7 @@ function calcBalls(arrBalls, maxWeight) {
     }, 0)
 
     const ballsProcent = arrBalls.reduce((acc, item) => {
-        acc.push(item * 100 / ballsSum)
+        acc.push((item * 100) / ballsSum)
         return acc
     }, [])
 
@@ -29,10 +30,10 @@ function calcBalls(arrBalls, maxWeight) {
     maxWeight = ballsSum < maxWeight ? ballsSum : maxWeight
     let totalLoot = 0
 
-    const ballsTheifBag = ballsProcent.map(item => {
-        totalLoot += Math.floor(maxWeight * item / 100)
+    const ballsTheifBag = ballsProcent.map((item) => {
+        totalLoot += Math.floor((maxWeight * item) / 100)
 
-        return Math.floor(maxWeight * item / 100)
+        return Math.floor((maxWeight * item) / 100)
     })
 
     if (totalLoot < maxWeight) {
@@ -43,24 +44,47 @@ function calcBalls(arrBalls, maxWeight) {
     return ballsTheifBag
 }
 
-function addBallsToBag(ballsTheifBag, ballsProcent, maxWeight, totalLoot,) {
-    const buffArrWithClearProcent = ballsProcent.map(item => {
-        const numb = maxWeight * item / 100
+function addBallsToBag(ballsTheifBag, ballsProcent, maxWeight, totalLoot) {
+    const buffArrWithClearProcent = ballsProcent.map((item) => {
+        const numb = (maxWeight * item) / 100
 
-        return (numb % 1 === 0) ? 0 : numb
+        return numb % 1 === 0 ? 0 : numb
     })
 
-
-
     while (maxWeight - totalLoot !== 0) {
-        const indexMostProcent = buffArrWithClearProcent.indexOf(Math.max(...buffArrWithClearProcent))
+        const maxProcent = Math.max(...buffArrWithClearProcent)
+        const indexMaxProcent = buffArrWithClearProcent.indexOf(maxProcent)
 
-        ballsTheifBag[indexMostProcent] += 1
-        buffArrWithClearProcent[indexMostProcent] = 0
-        totalLoot++
+        if (maxProcent < 1) {
+            ballsTheifBag[indexMaxProcent] += 1
+            buffArrWithClearProcent[indexMaxProcent] = 0
+            totalLoot++
+            continue
+        }
+
+        const currentProcent = buffArrWithClearProcent[indexMaxProcent]
+
+        if (currentProcent < Math.round(currentProcent)) {
+            ballsTheifBag[indexMaxProcent] += 1
+            buffArrWithClearProcent[indexMaxProcent] = 0
+            totalLoot++
+
+            checkSameProcent(maxProcent, buffArrWithClearProcent)
+        }
+
+        buffArrWithClearProcent[indexMaxProcent] = 0
     }
 }
 
 function chekZero(arrBalls, maxWeight) {
-    return (arrBalls === 0 || maxWeight === 0 || Math.max(...arrBalls) === 0) ? true : false
+    return arrBalls === 0 || maxWeight === 0 || Math.max(...arrBalls) === 0 ? true : false
+}
+
+function checkSameProcent(prevMaxProcent, buffArrWithClearProcent) {
+    const MaxProcent = Math.max(...buffArrWithClearProcent)
+
+    if (prevMaxProcent === MaxProcent) {
+        const index = buffArrWithClearProcent.indexOf(MaxProcent)
+        buffArrWithClearProcent[index] = 0
+    }
 }
